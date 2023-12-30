c11a229c3e6426aa89769c85a609cc2f89edc5cf
Mycroft Kang <thkang0629@gmail.com>
2023-12-30 15:22:53 +0900

    Update

--- src/lib/suikaGame/intro/index.tsx
+++ src/lib/suikaGame/intro/index.tsx
@@ -50,7 +50,7 @@ const Intro = ({isVisible, handleGameStart}: IntroProps) => {
         <button className={cx('btn')} onClick={onClick}>GAME START</button>
       </div>
 
-      <a href={'https://github.com/koreacat/suika-game#readme'} target='_blank' className={cx('patchLink')}>패치노트</a>
+      {/* <a href={'https://github.com/koreacat/suika-game#readme'} target='_blank' className={cx('patchLink')}>패치노트</a> */}
       <span className={cx('version')}>v1.0.5</span>
     </div>
   )
