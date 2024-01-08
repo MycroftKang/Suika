e97c4b5db6231cc027164d432ee31e5c98eb473f
Mycroft Kang <thkang0629@gmail.com>
2024-01-08 17:11:06 +0900

    Prevent landscape rotation

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,56 @@ window.addEventListener("beforeunload", (ev) =>
     return ev.returnValue = 'Are you sure you want to close?';
 });
 
+console.log(`The orientation of the screen is: ${window.screen.orientation.type}`);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+let firstLandscape = false;
+
+function handleRotation(first: boolean = false) {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile && window.screen.orientation.type === 'landscape-primary') {
+    let rootDiv = document.getElementById("root");
+    let screenDiv = document.getElementById("rotateScreen");
+    if (rootDiv) {
+      rootDiv.style.display = 'none';
+    }
+
+    if (screenDiv) {
+      screenDiv.style.display = 'flex';
+    }
+
+    if (first) {
+      firstLandscape = true;
+    }
+  }
+  else if (isMobile && window.screen.orientation.type === 'portrait-primary')
+  {
+    if (firstLandscape) {
+      window.location.reload();
+    }
+    
+    let rootDiv = document.getElementById("root");
+    let screenDiv = document.getElementById("rotateScreen");
+    if (rootDiv) {
+      rootDiv.style.display = 'block';
+    }
+
+    if (screenDiv) {
+      screenDiv.style.display = 'none';
+    }
+  }
+}
+
+handleRotation(true);
+
+window.addEventListener("orientationchange", () => {
+  console.log(`The orientation of the screen is: ${window.screen.orientation.type}`);
+  handleRotation();
+})
+
 
 root.render(<Test/>);
