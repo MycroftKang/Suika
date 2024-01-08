c485be07621557ddcb105d0233f5c34ff1b5985a
Mycroft Kang <thkang0629@gmail.com>
2024-01-08 17:52:43 +0900

    Prevent landscape rotation part 2

--- src/lib/suikaGame/object/Size.ts
+++ src/lib/suikaGame/object/Size.ts
@@ -1,16 +1,26 @@
+let renderHeight: number
+
 export const getRenderHeight = () => {
-    const maxHeight = Math.min(window.innerHeight - 80, 700);
-    const screenHeight = window.innerHeight;
-    const screenWidth = window.innerWidth - 8;
-    const maxWidth = screenHeight * 4 / 7 - 8;
   
-    if (maxWidth > screenWidth) {
-      return Math.min(maxHeight, (screenWidth) * 7 / 4);
-    }
-  
-    return Math.min(maxHeight, screenHeight);
-  };
-  
-  export const getRenderWidth = () => {
-    return getRenderHeight() * 4 / 7;
-  };
+  if (renderHeight) {
+    return renderHeight;
+  }
+
+  const maxHeight = Math.min(window.innerHeight - 80, 700);
+  const screenHeight = window.innerHeight;
+  const screenWidth = window.innerWidth - 8;
+  const maxWidth = screenHeight * 4 / 7 - 8;
+
+  if (maxWidth > screenWidth) {
+    renderHeight = Math.min(maxHeight, (screenWidth) * 7 / 4);
+  }
+  else {
+    renderHeight = Math.min(maxHeight, screenHeight);
+  }
+
+  return renderHeight;
+};
+
+export const getRenderWidth = () => {
+  return getRenderHeight() * 4 / 7;
+};
