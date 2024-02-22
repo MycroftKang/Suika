c905378dc9ac7a8a2c7908329531f4d8c47485da
Mycroft Kang <thkang0629@gmail.com>
2024-02-23 00:44:47 +0900

    Improve back button prevention

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,51 @@ import Test from './test';
 import {  } from "./firebase";
 
 // eslint-disable-next-line no-restricted-globals
-history.pushState(null, '', window.location.href);
+// history.pushState(null, '', window.location.href);
 
-const preventGoBack = () => {
-  console.log("pop");
-  const result = window.confirm("게임을 종료할까요?");
-  if (result) {
-    window.removeEventListener('popstate', preventGoBack);
-  } else { 
-    // eslint-disable-next-line no-restricted-globals  
+// const preventGoBack = () => {
+//   console.log("pop");
+//   const result = window.confirm("게임을 종료할까요?");
+//   if (result) {
+//     window.removeEventListener('popstate', preventGoBack);
+//   } else {
+//     // eslint-disable-next-line no-restricted-globals
+//     history.pushState(null, '', window.location.href);
+//   }
+// };
+  
+// window.addEventListener('popstate', preventGoBack);
+
+function isStandalone() {
+    return !!(navigator as any).standalone || window.matchMedia('(display-mode: standalone)').matches;
+}
+
+// Depends on bowser but wouldn't be hard to use a
+// different approach to identifying that we're running on Android
+function exitsOnBack () {
+  return isStandalone() && (window.navigator as any).userAgentData.platform === 'Android';
+}
+
+function handleBackEvents() {
+  
+  // eslint-disable-next-line no-restricted-globals
+  if (history.length < 2) {
+    // eslint-disable-next-line no-restricted-globals
     history.pushState(null, '', window.location.href);
   }
-};
   
-window.addEventListener('popstate', preventGoBack);
+  window.addEventListener('popstate', () => {
+    //TODO: Optionally show a "Press back again to exit" tooltip
+    setTimeout(() => {
+      // eslint-disable-next-line no-restricted-globals
+      history.pushState(null, '', window.location.href);
+      //TODO: Optionally hide tooltip
+    }, 2000);
+  });
+}
+
+if (exitsOnBack())
+  handleBackEvents();
 
 window.addEventListener("beforeunload", (ev) => 
 {  
