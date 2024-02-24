3b3bf6a69db01d78e6aa112db180f82e11b25b17
Mycroft Kang <thkang0629@gmail.com>
2024-02-24 10:30:53 +0900

    Fix screen freezing in Kakao in-app browser

--- src/lib/suikaGame/intro/index.tsx
+++ src/lib/suikaGame/intro/index.tsx
@@ -83,11 +83,13 @@ const Intro = ({isVisible, loadUserInfo,handleGameStart, handleShowRankModal}: I
   })
 
   const onClick = async () => {
-    window.addEventListener("beforeunload", (ev) => 
-    {  
-        ev.preventDefault();
-        return (ev.returnValue = 'Are you sure you want to close?');
-    });
+    if (!window.navigator.userAgent.toLowerCase().includes("kakaotalk")) {
+      window.addEventListener("beforeunload", (ev) => 
+      {
+          ev.preventDefault();
+          return (ev.returnValue = 'Are you sure you want to close?');
+      });
+    }
 
     handleGameStart();
   }
